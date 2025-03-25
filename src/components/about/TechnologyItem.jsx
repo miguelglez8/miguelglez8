29f5bcb58dd340668d33f0dd857123db4c870e4d
Miguel González Navarro <UO282337@uniovi.es>
2025-03-25 10:30:23 +0100

    Update TechnologyItem.jsx

--- src/components/about/TechnologyItem.jsx
+++ src/components/about/TechnologyItem.jsx
@@ -1,18 +1,46 @@
-import {technologiesColors} from "../../data/colors.js";
+import { technologiesColors } from "../../data/colors.js";
 
-const TechnologyItem = ({ name }) => (
-    <span
-        style={{
-            backgroundColor: technologiesColors[name] || "#ccc",
-            color: "#fff",
-            padding: "8px 16px",
-            borderRadius: "20px",
-            fontSize: "14px",
-            fontWeight: "bold",
-        }}
-    >
-        {name}
+import { FaDocker, FaJava, FaAws, FaReact, FaGit, FaGithub, FaRegCheckCircle, FaCogs } from 'react-icons/fa';
+import { SiSpringboot, SiFirebase, SiKotlin, SiAndroid, SiHtml5, SiJavascript, SiMysql, SiMongodb } from 'react-icons/si';
+
+const logoMapping = {
+    JavaScript: <SiJavascript />,
+    React: <FaReact />,
+    HTML: <SiHtml5 />,
+    Java: <FaJava />,
+    "Spring Boot": <SiSpringboot />,
+    Docker: <FaDocker />,
+    AWS: <FaAws />,
+    Firebase: <SiFirebase />,
+    MySQL: <SiMysql />,
+    MongoDB: <SiMongodb />,
+    Git: <FaGit />,
+    GitHub: <FaGithub />,
+    Android: <SiAndroid />,
+    Kotlin: <SiKotlin />,
+    JUnit: <FaRegCheckCircle />,
+    JMeter: <FaCogs />,
+};
+
+const TechnologyItem = ({ name }) => {
+    return (
+        <span
+            style={{
+                backgroundColor: technologiesColors[name] || "#ccc",
+                color: "#fff",
+                padding: "8px 16px",
+                borderRadius: "20px",
+                fontSize: "14px",
+                fontWeight: "bold",
+                display: "flex",
+                alignItems: "center",
+                gap: "8px",
+            }}
+        >
+            {logoMapping[name]}
+            <span>{name}</span>
     </span>
-);
+    );
+};
 
 export default TechnologyItem;
