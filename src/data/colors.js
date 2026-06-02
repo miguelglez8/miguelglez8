e6ee88203c6c1d8bc1e4b55b08d8d8096656eac3
Miguel Gonzalez Navarro <UO282337@uniovi.es>
2026-06-02 18:18:00 +0200

    Update colors.js

--- src/data/colors.js
+++ src/data/colors.js
@@ -1,13 +1,17 @@
 export const technologiesColors = {
     java: "#007396",
     spring_boot: "#6DB33F",
+    react: "#61DAFB",
+    javascript: "#F7DF1E",
     rest_apis: "#4A90E2",
     microservices: "#9B59B6",
-    docker: "#0DB7ED",
-    git: "#FF0000",
-    github: "#333333",
+    kafka: "#000000",
     sql: "#00758F",
-    aws: "#FF9900",
+    git: "#E44C30",
+    docker: "#2496ED",
+    junit: "#25A162",
+    jsp: "#007396",
+    liferay:"#0B5FFF"
 };
 
 export const statusColors = {
@@ -19,15 +23,15 @@ export const statusColors = {
 export const techColors = {
     React: "#61DAFB",
     JavaScript: "#F7DF1E",
-    "GitHub Pages": "#333",
-    Spring: "#6DB33F",
-    Docker: "#0DB7ED",
+    "GitHub Pages": "#100000",
+    "Spring Boot": "#6DB33F",
+    Docker: "#2496ED",
     AWS: "#FF9900",
     Kotlin: "#7F52FF",
     Java: "#007396",
-    MongoDB: "#4DB33D",
-    Node: "#2D7D2A",
-    SOLID: "#E91E63",
+    MongoDB: "#4EA94B",
+    Node: "#6DA55F",
+    SOLID: "#9500BB",
     "CI/CD": "#00BCD4"
 };
 
@@ -37,6 +41,3 @@ export const linkColors = {
     doc: "#007BFF",
     demo: "#FF0000"
 };
-
-
-
